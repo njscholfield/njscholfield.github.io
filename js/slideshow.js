31df8f6235291e76cbce15079d6db4accddbf332
Noah Scholfield <noah.scholfield@gmail.com>
2015-12-13 16:39:32 -0500

    Try again

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,8 +1,6 @@
 (function waitForElement(){
     if(typeof Galleria !== "undefined"){
-      if(ratioShouldWork == true){
         loadGalleria();
-      }
     }else{
         setTimeout(function() { waitForElement(); }, 250);
     }
@@ -11,10 +9,10 @@
 function loadGalleria() {
   Galleria.loadTheme('/js/galleria/classic/galleria.classic.min.js');
   Galleria.run('#testGalleria', {
-    height: 0.75,
     autoplay: true,
     thumbnails: false,
     showCounter: false,
+    height: 0.75,
     flickr: 'set:72157635984432836',
     flickrOptions: {
       max: 100,
