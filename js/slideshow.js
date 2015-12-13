eed01398a1e52b7a2f73b7f018d48e1c956be034
Noah Scholfield <noah.scholfield@gmail.com>
2015-12-13 03:52:30 -0500

    Remove incorrect this.bind()

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,14 +1,12 @@
-this.bind(Galleria, function() {
-  Galleria.loadTheme('/js/galleria/classic/galleria.classic.min.js');
-  Galleria.run('#testGalleria', {
-    height: 0.75,
-    autoplay: true,
-    thumbnails: false,
-    showCounter: false,
-    flickr: 'set:72157635984432836',
-    flickrOptions: {
-      max: 100,
-      description: true
-    }
-  });
+Galleria.loadTheme('/js/galleria/classic/galleria.classic.min.js');
+Galleria.run('#testGalleria', {
+  height: 0.75,
+  autoplay: true,
+  thumbnails: false,
+  showCounter: false,
+  flickr: 'set:72157635984432836',
+  flickrOptions: {
+    max: 100,
+    description: true
+  }
 });
