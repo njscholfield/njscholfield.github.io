09fa90d78e8715d89db98d98ab2f88e86098bff4
Noah Scholfield <noah.scholfield@gmail.com>
2015-12-13 03:04:50 -0500

    Try to make work with Rocket Loader

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,12 +1,14 @@
-Galleria.loadTheme('/js/galleria/classic/galleria.classic.min.js');
-Galleria.run('#testGalleria', {
-  height: 0.75,
-  autoplay: true,
-  thumbnails: false,
-  showCounter: false,
-  flickr: 'set:72157635984432836',
-  flickrOptions: {
-    max: 100,
-    description: true
-  }
+this.bind(Galleria, function() {
+  Galleria.loadTheme('/js/galleria/classic/galleria.classic.min.js');
+  Galleria.run('#testGalleria', {
+    height: 0.75,
+    autoplay: true,
+    thumbnails: false,
+    showCounter: false,
+    flickr: 'set:72157635984432836',
+    flickrOptions: {
+      max: 100,
+      description: true
+    }
+  });
 });
