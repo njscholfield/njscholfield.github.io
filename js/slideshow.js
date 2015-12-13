e305bdb9e3962338f456318a388912e6a9c38b3f
Noah Scholfield <noah.scholfield@gmail.com>
2015-12-13 17:05:33 -0500

    Restore to original

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,22 +1,12 @@
-(function waitForElement(){
-    if(typeof Galleria !== "undefined"){
-        loadGalleria();
-    }else{
-        setTimeout(function() { waitForElement(); }, 250);
-    }
-})()
-
-function loadGalleria() {
-  Galleria.loadTheme('/js/galleria/classic/galleria.classic.min.js');
-  Galleria.run('#testGalleria', {
-    autoplay: true,
-    thumbnails: false,
-    showCounter: false,
-    height: 0.75,
-    flickr: 'set:72157635984432836',
-    flickrOptions: {
-      max: 100,
-      description: true
-    }
-  });
-}
+Galleria.loadTheme('/js/galleria/classic/galleria.classic.min.js');
+Galleria.run('.galleria', {
+  height: 0.75,
+  autoplay: true,
+  thumbnails: false,
+  showCounter: false,
+  flickr: 'set:72157635984432836',
+  flickrOptions: {
+    max: 100,
+    description: true
+  }
+});
