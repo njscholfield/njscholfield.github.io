4eed83121fb6dd453acd62ce7c43d7e226541c35
Noah Scholfield <noah.scholfield@gmail.com>
2015-12-13 00:44:46 -0500

    Try moving Galleria initialization to separate file

---
+++ js/slideshow.js
@@ -0,0 +1,12 @@
+Galleria.loadTheme('/js/galleria/classic/galleria.classic.min.js');
+Galleria.run('#testGalleria', {
+  height: 0.75,
+  autoplay: true,
+  thumbnails: false,
+  showCounter: false,
+  flickr: 'set:72157635984432836',
+  flickrOptions: {
+    max: 100,
+    description: true
+  }
+});
