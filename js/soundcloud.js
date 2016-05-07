d710d950acdefdf96962cb2999bb1161d87f4d3e
Noah Scholfield <contact@noahscholfield.com>
2016-05-07 13:17:25 -0400

    Try older SoundCloud SDK

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -1,8 +1,1 @@
-SC.initialize({
-  client_id: '30cba84d4693746b0a2fbc0649b2e42c'
-})
-
-SC.resolve('https://soundcloud.com/tritoniaradio/tritonia-128').then(function(tracks) {
-  //alert(JSON.stringify(tracks))
-  alert(tracks.description)
-})
+console.log("Test")
