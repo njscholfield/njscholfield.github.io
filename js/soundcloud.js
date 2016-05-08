87ce9e0781349178781cb9bbeee0e36d9914daee
Noah Scholfield <contact@noahscholfield.com>
2016-05-08 03:26:45 -0400

    Create UI, works in Chrome, but not Safari

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -1,1 +1,18 @@
-console.log("Test")
+var app = angular.module("soundcloud", [])
+var scapi = "http://api.soundcloud.com/resolve.json?url="
+var client = "&client_id=30cba84d4693746b0a2fbc0649b2e42c"
+
+app.controller("descriptionController", ["$http", function($http) {
+  var sc = this
+  sc.submit = function() {
+    var callURL = scapi + sc.url + client
+    console.log(callURL)
+    $http.get(callURL)
+      .success(function(response) {
+        sc.trackJSON = response
+      })
+      .error(function(response) {
+        console.log(response)
+      })
+  }
+}])
