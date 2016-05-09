d3a3bb174d956de922f9c0fbf31fdeecee0f0348
Noah Scholfield <contact@noahscholfield.com>
2016-05-09 03:24:19 -0400

    Rename to index.html

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -7,12 +7,9 @@ app.controller("descriptionController", ["$http", function($http) {
   sc.showJSON = false
   sc.submit = function() {
     var callURL = scapi + sc.url + client
-    console.log(callURL)
     $http.get(callURL)
       .success(function(response) {
-        console.log(response)
         sc.trackJSON = response
-        console.log(sc.trackJSON)
       })
       .error(function(response) {
         console.log(response)
