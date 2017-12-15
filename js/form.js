8af409ecb0878be6cc5a1cfd0d8354200e8e8594
Noah Scholfield <contact@noahscholfield.com>
2017-12-14 21:47:10 -0500

    Add reCAPTCHA

--- js/form.js
+++ js/form.js
@@ -4,6 +4,12 @@ const form = document.querySelector('#contact-form');
 const name = document.querySelector('#name');
 const email = document.querySelector('#email');
 const message = document.querySelector('#message');
+const grecap = document.querySelector('.g-recaptcha');
+let passedRecaptcha;
+
+function recaptcha(token) {
+  passedRecaptcha = (token) ? true: false;
+}
 
 function showBanner(banner) {
   setTimeout(() => banner.classList.remove('hidden'), 500);
@@ -12,6 +18,11 @@ function showBanner(banner) {
 
 function sendResponse(e) {
   e.preventDefault();
+  if(!passedRecaptcha) {
+    grecap.classList.add('red-border');
+    return;
+  }
+  grecap.classList.remove('red-border');
   var body = 'entry.1687361453=' + name.value + '&entry.597865152=' + email.value + '&entry.142010775=' + message.value;
   fetch('https://docs.google.com/forms/d/1TqUYtC2s8Uv5ChXPEqMGm1GZTyGNn0Wto2Mb2biIMHE/formResponse', {
     method: 'POST',
