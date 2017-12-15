4bd50ce406ce87a4ebdf4adae064dbf2d93656e4
Noah Scholfield <contact@noahscholfield.com>
2017-12-14 20:40:23 -0500

    Fix contact form

--- js/form.js
+++ js/form.js
@@ -1,19 +1,28 @@
-var icount = 0;
+const errorBanner = document.querySelector('#failure');
+const successBanner = document.querySelector('#success');
+const form = document.querySelector('#contact-form');
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const message = document.querySelector('#message');
 
-function confirm() {
-  setTimeout(function() {
-    if (icount > 1) {
-      $("#success").removeClass("hidden");
-      document.getElementById("contact-form").reset();
-      icount = 1;
-    }
-    else{
-      $("#failure").removeClass("hidden");
-    }
-  }, 500);
+function showBanner(banner) {
+  setTimeout(() => banner.classList.remove('hidden'), 500);
+  setTimeout(() => banner.classList.add('hidden'), 3000);
+}
 
-  setTimeout(function() {
-    $("#success").addClass("hidden");
-    $("#failure").addClass("hidden");
-  }, 3000);
-};
+function sendResponse(e) {
+  e.preventDefault();
+  var body = 'entry.1687361453=' + name.value + '&entry.597865152=' + email.value + '&entry.142010775=' + message.value;
+  fetch('https://docs.google.com/forms/d/1TqUYtC2s8Uv5ChXPEqMGm1GZTyGNn0Wto2Mb2biIMHE/formResponse', {
+    method: 'POST',
+    body: body,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    },
+    mode: 'no-cors'
+  })
+    .then(() => {showBanner(successBanner); form.reset();})
+    .catch(() => showBanner(errorBanner));
+}
+
+form.addEventListener('submit', sendResponse);
